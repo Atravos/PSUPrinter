6dc0f69dc9c33b9ddae86a04eaf185b3eeec9300
Hunter Walker <hunterwalker@Hunters-MacBook-Pro.local>
2021-02-06 19:58:19 -0600

    Yet another commit

---
+++ PSUPrinter.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) 
+  {
+
+    Scanner keyboard = new Scanner(System.in);
+      Scanner user_input = new Scanner(System.in);
+    
+    System.out.println( " PPPP          SSSSSS        U    U");
+    System.out.println( " P    P        S             U    U");
+    System.out.println( " PPPP          SSSSSS        U    U");
+    System.out.println( " P                  S        U    U");
+    System.out.println( " P             SSSSSS        UUUUUU");
+    
+  }
+}
